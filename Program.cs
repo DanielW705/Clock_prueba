0850a3403dc969c3b1d639e250c250de7bc58387
Dano <Danielmtzlpz95@gmail.com>
2023-08-01 18:27:03 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Clock_prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se inicializa el reloj
+            Clock clock = new Clock();
+            //Se inizializa el temporizador
+            Timer timer = new Timer();
+            //Se pasa la funcion que se accionara con el evento
+            clock.Tick += timer.OnSecondPassed;
+            //Se mantendra en bucle para que este siempre muestre el tiempo
+            while (true)
+                clock.SecondPassed();
+        }
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,16 @@
+﻿namespace Clock_prueba
+{
+    public class Timer
+    {
+        //Esta es la funcion de cuando pase cierto tiempo
+        public void OnSecondPassed(object source, EventArgs e)
+        {
+            //Se inciara una variable que obtendra el tiempo de manera actual
+            DateTime date = DateTime.Now;
+            //Limpiara la consola 
+            Console.Clear();
+            //Escribira de nuevo el tiempo actual
+            Console.WriteLine($"{date.ToString("HH:mm:ss")}");
+        }
+    }
+}
